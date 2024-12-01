ba4d9ef1fc59c1634b2b1ed3b72ba582a95e25c8
RokHyung Son <fhrgud1264@gmail.com>
2024-12-01 18:07:48 +0900

    refactor: Change install command by new image    note 1: Remove chrome-deps.txt    note 2: Optimize install-browser.sh    note 3: Remove unnecessary command at Dockerfile

--- Dockerfile
+++ Dockerfile
@@ -1,23 +1,15 @@
-FROM public.ecr.aws/lambda/python:3.13 as stage
+FROM public.ecr.aws/lambda/python:3.13
 
-RUN yum install -y -q sudo unzip
+RUN dnf install -y unzip wget
 
 # Install Chromium
 COPY install-browser.sh /tmp/
-RUN /usr/bin/bash /tmp/install-browser.sh
-
-FROM public.ecr.aws/lambda/python:3.13 as base
-
-COPY chrome-deps.txt /tmp/
-RUN yum install -y $(cat /tmp/chrome-deps.txt)
+RUN /bin/sh /tmp/install-browser.sh
 
 # Install Python dependencies for function
 COPY requirements.txt /tmp/
 RUN python3 -m pip install --upgrade pip -q
 RUN python3 -m pip install -r /tmp/requirements.txt -q
-
-COPY --from=stage /opt/chrome /opt/chrome
-COPY --from=stage /opt/chromedriver /opt/chromedriver
 
 # copy main.py
 COPY main.py /var/task/
--- install-browser.sh
+++ install-browser.sh
@@ -1,14 +1,12 @@
 #!/bin/bash
 
-
 echo "Downloading Chromium..."
 CHROMIUM_ROOT="http://commondatastorage.googleapis.com/chromium-browser-snapshots/Linux_x64"
 CHROMIUM_LATEST=`wget -q -O - "$CHROMIUM_ROOT/LAST_CHANGE"`
-wget $CHROMIUM_ROOT/$CHROMIUM_LATEST/chrome-linux.zip
-unzip /tmp/chromium.zip -d /tmp/
+wget $CHROMIUM_ROOT/$CHROMIUM_LATEST/chrome-linux.zip -P /tmp/
+unzip /tmp/chrome-linux.zip -d /tmp/
 mv /tmp/chrome-linux/ /opt/chrome
 
-CHROMIUM_ROOT="http://commondatastorage.googleapis.com/chromium-browser-snapshots/Linux_x64"
-wget $CHROMIUM_ROOT/$CHROMIUM_LATEST/chromedriver_linux64.zip
+wget $CHROMIUM_ROOT/$CHROMIUM_LATEST/chromedriver_linux64.zip -P /tmp/
 unzip /tmp/chromedriver_linux64.zip -d /tmp/
 mv /tmp/chromedriver_linux64/chromedriver /opt/chromedriver
