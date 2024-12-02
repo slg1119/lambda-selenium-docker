43f5f3839d9b422df1a953f705f012e310ae1882
RokHyung Son <fhrgud1264@gmail.com>
2024-12-02 17:17:13 +0900

    refactor: Change Chrome options and Install chromedriver automatically

--- main.py
+++ main.py
@@ -1,30 +1,51 @@
 import json
+
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
 
 
 def handler(event=None, context=None):
     chrome_options = webdriver.ChromeOptions()
-    chrome_options.binary_location = "/opt/chrome/chrome"
-    chrome_options.add_argument("--headless")
+    
+    # 기본 설정
+    chrome_options.add_argument('--headless=new')
     chrome_options.add_argument('--no-sandbox')
-    chrome_options.add_argument("--single-process")
-    chrome_options.add_argument("--disable-dev-shm-usage")
-    chrome_options.add_argument("user-agent=Mozilla/5.0 (Windows NT 6.1; WOW64; Trident/7.0; rv:11.0) like Gecko")
-    chrome_options.add_argument('window-size=1392x1150')
-    chrome_options.add_argument("disable-gpu")
-    service = Service("/opt/chromedriver")
-    browser = webdriver.Chrome(service=service, options=chrome_options)
+    chrome_options.add_argument('--disable-dev-shm-usage')
+    chrome_options.add_argument('--disable-gpu')
+    chrome_options.add_argument('--disable-software-rasterizer')
+    
+    # DevTools 관련 설정
+    chrome_options.add_argument('--disable-extensions')
+    chrome_options.add_argument('--disable-dev-tools')
+    
+    # 메모리 및 성능 설정
+    chrome_options.add_argument('--disable-features=NetworkService')
+    chrome_options.add_argument('--disable-features=VizDisplayCompositor')
+    chrome_options.add_argument('--force-device-scale-factor=1')
+    
+    # tmp 디렉토리 활용
+    chrome_options.add_argument(f'--user-data-dir=/tmp/chrome/user-data')
+    chrome_options.add_argument(f'--crash-dumps-dir=/tmp/chrome/crashes')
+    chrome_options.add_argument('--disk-cache-dir=/tmp/chrome/cache')
+    
+    # 추가 안정성 설정
+    chrome_options.add_argument('--ignore-certificate-errors')
+    chrome_options.add_argument('--no-first-run')
+    chrome_options.add_argument('--window-size=1920,1080')
 
-    print("browser", browser)
+    chrome_options.binary_location = "/usr/bin/google-chrome-stable"
+    service = Service(ChromeDriverManager().install())
+    driver = webdriver.Chrome(service=service, options=chrome_options)
 
-    browser.get("https://www.naver.com/")
-    description = browser.find_element(By.NAME, "description").get_attribute("content")
+
+    driver.get("https://www.naver.com/")
+    description = driver.find_element(By.NAME, "description").get_attribute("content")
 
     print(description)
 
-    browser.close()
+    driver.quit()
 
     return {
         "statusCode": 200,
